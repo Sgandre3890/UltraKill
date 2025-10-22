d5e5f221dcd52365231c1e0ca8182972fce8c90c
RagingVirus32 <erikcamd@gmail.com>
2025-10-22 17:32:49 -0600

    Repositioned WIN namespace
    -moved glfw window namespace out of main.cpp into it's own file, included window.h.
    -Added "headers" folder to CMake include directory

---
+++ Include/headers/window.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace WIN {
+
+    extern GLFWwindow* window;
+    extern const unsigned int SCR_WIDTH;
+    extern const unsigned int SCR_HEIGHT;
+
+    int initializeWindow(int SCR_WIDTH, int SCR_HEIGHT, const char* title);
+    void terminateWindow();
+    void processInput(GLFWwindow* window);
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+}
