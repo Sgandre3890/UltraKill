86ad6e93bf96db22012cfac62cec5a07730164a2
Sean G <seangand9606@graniteschools.org>
2025-10-21 20:37:26 -0600

    update

--- compile.bat
+++ compile.bat
@@ -2,3 +2,4 @@ cd build
 cmake --build .
 cd ..\debug
 out.exe
+cd ..
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,76 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;   
+
+namespace Window {
+    GLFWwindow* window = nullptr;
+
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+    {
+        glViewport(0, 0, width, height);
+    }
+
+    void processInput(GLFWwindow *window)
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+    }
+
+    int initializeWindow(int SCR_WIDTH, int SCR_HEIGHT, const char* title)
+    {
+        if (!glfwInit()) {
+            std::cout << "Failed to initialize GLFW" << std::endl;
+            return -1;
+        }
+
+        // Setup GLFW
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, title, NULL, NULL);
+        if (window == NULL)
+        {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            return -1;
+        }
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            return -1;
+        }
+        return 0;
+    }
+
+    void terminateWindow()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+}
+
 int main() {
 
-    // Initialize GLFW
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    if (Window::initializeWindow(SCR_WIDTH, SCR_HEIGHT, "UltraKill") != 0) 
+        return -1;
 
+    while(!glfwWindowShouldClose(Window::window)) {
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        Window::processInput(Window::window);
+        glfwSwapBuffers(Window::window);
+        glfwPollEvents();
+    }
+    
+   
     return 0;
 }
+
